1a19f40c6ccf22c05c5e9e6ae50bbd1ef9e73d4f
mohand <myounsipro@gmail.com>
2023-11-04 14:45:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './my.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Belcaricature
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Belkacem YOUNSI
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
