044d262547459701186552c6eb586264cf531781
mohand <myounsipro@gmail.com>
2023-11-04 17:42:58 +0100

    9

---
+++ src/components/contact/Contact.js
@@ -0,0 +1,23 @@
+import './Contact.css';
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <header className="Contact-header">
+        <p>
+          Belcaricature
+        </p>
+        <a
+          className="Contact-link"
+          href="https://www.facebook.com/people/belkacem-caricatures/100050603164367/"
+          target="_target"
+          rel="noopener noreferrer"
+        >
+          Contactez Moi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Contact;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Contact from './components/contact/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Contact />
   </React.StrictMode>
 );
 
